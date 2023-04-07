5de70c62e2e3b39c1e6399ad823a4a78054d72a8
Dustin Stringer <dustin.stringer@us.af.mil>
2023-04-07 17:57:48 -0400

    added gpt4 integration

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const { generateResponse } = require("./gpt4");
+const { generateResponse, generateChatResponse } = require("./gpt4");
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -15,6 +17,38 @@ app.post("/api/ai/insights", async (req, res) => {
     res.json({ insight: response });
   } catch (error) {
     res.status(500).json({ error: "Error generating AI insight" });
+  }
+});
+
+/**
+ * Integrates a body of text between two contextual texts
+ */
+app.post("/api/ai/integrate", async (req, res) => {
+  const { text, startContext, endContext } = req.body;
+  const messages = [
+    {
+      role: "system",
+      content:
+        "Seamlessly integrate the given text into the given context so that the verbiage, tone, and style are consistent with the context. Only return the integrated text. Do not return the context. Do not return any explanation of how the text was integrated.",
+    },
+    {
+      role: "user",
+      content: `Given the following text: ${text}
+    
+    Seamlessly integrate the text into the given context so that the verbiage, tone, and style are consistent with the context. Only return the integrated text. Do not return the context. Do not return any explanation of how the text was integrated.
+
+    ${startContext}
+
+    INSERT TEXT HERE
+    
+    ${endContext}`,
+    },
+  ];
+  try {
+    const response = await generateChatResponse(messages);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error integrating text" });
   }
 });
 
--- gpt4.js
+++ gpt4.js
@@ -1,26 +1,104 @@
-const openai = require("openai");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-// OpenAI key from environment variable
-openai.apiKey = process.env.OPENAI_API_KEY;
+/**
+ * Generates a response from a prompt
+ * @param {string} prompt The prompt to generate a response from
+ * @param {string} model The model to use
+ * @param {number} responses The number of responses to generate
+ * @returns {string} The generated response
+ * @throws {Error} If the prompt is invalid
+ */
+async function generateResponse(prompt, model = "gpt-4", responses = 1) {
+  // Verify that prompt is a string
+  if (typeof prompt !== "string") {
+    throw new Error("Invalid prompt");
+  }
 
-async function generateResponse(prompt) {
+  // Verify the model
+  if (!verifyModel(model)) {
+    throw new Error("Invalid model");
+  }
+
+  // Verify that responses is a number
+  if (typeof responses !== "number") {
+    throw new Error("Invalid responses");
+  }
+
   try {
-    const result = await openai.Completion.create({
-      engine: "text-davinci-002", // You can use other engines like 'text-curie-002', 'text-babbage-002', etc.
-      prompt: prompt,
-      max_tokens: 100,
-      n: 1,
-      stop: null,
-      temperature: 0.7,
+    const completion = await openai.createChatCompletion({
+      model,
+      messages: [{ role: "user", content: prompt }],
+      n: responses,
     });
-
-    return result.choices[0].text;
+    console.log(completion.data.choices);
+    return completion.data.choices[0].message.content;
   } catch (error) {
     console.error("Error generating response:", error);
     return "";
   }
 }
 
-module.exports = {
-  generateResponse,
-};
+/**
+ * Generates a response from a series of chat messages
+ * @param {object[]} messages An array of prompt messages
+ * @param {number} responses The number of responses to generate
+ * @returns {object[]} An array of messages
+ */
+async function generateChatResponse(messages, model = "gpt-4", responses = 1) {
+  // Verify that messages is an array of objects, with role and content keys and string values
+  if (
+    !Array.isArray(messages) ||
+    messages.some(
+      (message) =>
+        typeof message !== "object" ||
+        typeof message.role !== "string" ||
+        typeof message.content !== "string"
+    )
+  ) {
+    throw new Error("Invalid messages");
+  }
+
+  // Verify the model
+  if (!verifyModel(model)) {
+    throw new Error("Invalid model");
+  }
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages,
+      n: responses,
+    });
+    console.log(completion.data);
+    return completion.data.choices;
+  } catch (error) {
+    console.error("Error generating response:", error);
+    return "";
+  }
+}
+
+function verifyModel(model) {
+  // Verify that model is a string of a valid model
+  const validChatModels = [
+    "gpt-4",
+    "gpt-4-0314",
+    "gpt-4-32k",
+    "gpt-4-32k-0314",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0301",
+    "text-davinci-003",
+    "text-davinci-002",
+    "code-davinci-002",
+  ];
+  if (typeof model !== "string" || !validChatModels.includes(model)) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = { generateResponse, generateChatResponse };
