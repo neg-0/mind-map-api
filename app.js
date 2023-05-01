133d599ee62a30f01fd6e89d9ddf101c19cf85a4
Dustin Stringer <dustin.stringer@us.af.mil>
2023-05-01 19:31:51 -0400

    implemented logging and websockets for persistence

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const { generateResponse, generateChatResponse } = require("./gpt4");
 const express = require("express");
+const http = require("http");
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const logger = require("./logger");
+
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server });
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  logger.info("GET request to root endpoint");
 });
 
 app.post("/api/ai/insights", async (req, res) => {
@@ -52,6 +58,51 @@ app.post("/api/ai/integrate", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Initialize the data
+let nodes = []; // Replace with your initial nodes data
+let edges = []; // Replace with your initial edges data
+
+// Broadcast a message to all connected clients
+function broadcast(message) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+wss.on("connection", (socket) => {
+  logger.info("Client connected");
+
+  // Send the current nodes and edges to the newly connected client
+  socket.send(JSON.stringify({ type: "nodes-update", nodes }));
+  socket.send(JSON.stringify({ type: "edges-update", edges }));
+
+  // Listen for messages from clients
+  socket.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    switch (data.type) {
+      case "nodes-update":
+        logger.info("Received nodes update");
+        nodes = data.nodes;
+        broadcast(JSON.stringify({ type: "nodes-update", nodes }));
+        break;
+      case "edges-update":
+        logger.info("Received edges update");
+        edges = data.edges;
+        broadcast(JSON.stringify({ type: "edges-update", edges }));
+        break;
+      // Handle other message types as needed
+    }
+  });
+
+  socket.on("close", () => {
+    logger.info("Client disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  logger.info(`Example app listening on port ${PORT}`);
 });
